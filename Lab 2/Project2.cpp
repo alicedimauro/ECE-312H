6b8092a044174883d498a3c32d938904f10a0d65
alicedimauro <alicedimauro@utexas.edu>
2025-02-04 21:55:26 -0600

    Create PA2 file and folder

---
+++ Lab 2/Project2.cpp
@@ -0,0 +1,40 @@
+// matrix-multiplication <Project2.cpp>
+// ECE 312 Project 2 submission by
+// <Your Name Here>
+// <Your EID>
+// Slip days used: <0>
+// Fall 2023
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "MatrixMultiply.h"
+
+void multiplyMatrices(
+        double a[],
+        const uint32_t a_rows,
+        const uint32_t a_cols,
+        double b[],
+        const uint32_t b_cols,
+        double c[]) {
+    // https://en.wikipedia.org/wiki/Row-_and_column-major_order
+}
+
+double** multiplyMatricesPtr(
+        double** a,
+        const uint32_t a_rows,
+        const uint32_t a_cols,
+        double** b,
+        const uint32_t b_cols) {
+    return NULL;
+}
+
+// https://en.wikipedia.org/wiki/Matrix_(mathematics)#Submatrix
+double** createSubMatrix(
+         double** a,
+         const uint32_t a_rows,
+         const uint32_t a_cols,
+		 const uint32_t row_x, 
+		 const uint32_t col_y) {
+    return NULL;
+}
