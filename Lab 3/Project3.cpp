52287ac6f36b466a88295c781c0ef56082705978
Alice Di Mauro <alicedimauro@utexas.edu>
2025-02-22 16:17:59 -0600

    Fixed syntax errors

--- Lab 3/Project3.cpp
+++ Lab 3/Project3.cpp
@@ -29,7 +29,7 @@ void utqueueprint(UTQueue *src) {
  * Initialize the queue correctly by copying src
  * Return a pointer to the newly created UTQueue.
  */
-UTQueue *utqueuedup(const int *src, int n) 
+UTQueue *utqueuedup(const int *src, int n) {
 // { return NULL; }
 
 UTQueue *queue = (UTQueue *)malloc(sizeof(UTQueue));
@@ -41,14 +41,16 @@ for (int i = 0; i < n; i++) {
     queue->data[i] = src[i];
 }
 return queue;
+}
 
 /*
  * Free all memory associated with the given UTQueue.
  */
-void utqueuefree(UTQueue *self) {}
+void utqueuefree(UTQueue *self) {
 if (self) {
  free(self->data);
  free(self);
+}
 }
 
 
@@ -56,13 +58,14 @@ if (self) {
  * Push (enqueue) an integer "value" into the queue.
  */
 
-UTQueue *utqueuepush(UTQueue *src, int value) 
+UTQueue *utqueuepush(UTQueue *src, int value) {
 //{ return NULL; }
 src->data = (int *)realloc(src->data, (src->size + 1) * sizeof(int));
     assert(src->data != NULL);
     src->data[src->size] = value;
     src->size++;
     return src;
+}
 
 /*
  * Pop (dequeue) an element from the queue
@@ -86,7 +89,7 @@ for (int i = 1; i < src->size; i++) {
 /*
  * Reverses the queue in src.
  */
-UTQueue *utqueuerev(UTQueue *src) 
+UTQueue *utqueuerev(UTQueue *src) {
 //{ return NULL; } Put this back? But function should return pointer no?
 for (int i = 0, j = src->size - 1; i < j; i++, j--) {
   int temp = src->data[i];
@@ -94,13 +97,14 @@ for (int i = 0, j = src->size - 1; i < j; i++, j--) {
   src->data[j] = temp;
 }
 return src;
+}
 
 
 /*
  * combines the elements of UTQueues dst and src INTO dst.
  * src should be deleted after the combination
  */
-UTQueue *utqueuecombine(UTQueue *dst, UTQueue *src) 
+UTQueue *utqueuecombine(UTQueue *dst, UTQueue *src) {
 // { return NULL; }
 dst->data = (int *)realloc(dst->data, (dst->size + src->size) * sizeof(int));
     assert(dst->data != NULL);
@@ -110,6 +114,7 @@ dst->data = (int *)realloc(dst->data, (dst->size + src->size) * sizeof(int));
     dst->size += src->size;
     utqueuefree(src);
     return dst;
+}
 
 
 /*
@@ -117,10 +122,11 @@ dst->data = (int *)realloc(dst->data, (dst->size + src->size) * sizeof(int));
  * q1 should "have" the contents of q2 and vice verse.
  * There is more than one way to do this
  */
-void utqueueswap(UTQueue *q1, UTQueue *q2) {}
+void utqueueswap(UTQueue *q1, UTQueue *q2) {
 UTQueue temp = *q1;
     *q1 = *q2;
     *q2 = temp;
+}
 
 /*
  * Copy values from src array into dst.
@@ -131,11 +137,12 @@ UTQueue temp = *q1;
  * If all of src can fit into dst, leave the remaining ints in dst.
  * return the pointer to dst with the changes.
  */
-UTQueue *utqueuecpy(UTQueue *dst, const int *src, int n)
+UTQueue *utqueuecpy(UTQueue *dst, const int *src, int n) {
 // { return NULL; }
 int minSize = (dst->size < n) ? dst->size : n;
     for (int i = 0; i < minSize; i++) {
         dst->data[i] = src[i];
     }
     return dst;
+}
 
