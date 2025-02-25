5fc602cf1e4cd2282bd7719f3319f7096cd100e8
alicedimauro <alicedimauro@utexas.edu>
2025-02-25 17:50:02 -0600

    initialize lab

---
+++ Lab 4/Project4.cpp
@@ -0,0 +1,29 @@
+// ECE 312(H) PA4
+// <Your Name Here>
+// <Your EID Here>
+// Spring 2025
+// Speight 
+
+#include "Invent.h"
+#include "MyString.h"
+#include <assert.h>
+#include <stdio.h>
+
+#define MAX_CUSTOMERS 1000
+Customer customers[MAX_CUSTOMERS];
+int num_customers = 0;
+
+/* you'll probably need several more global variables */
+/* and of course, you have a few functions to write */
+
+/* clear the inventory and reset the customer database to empty */
+void reset(void) { /* your code here */
+}
+
+void processSummarize() {}
+
+void processPurchase() {}
+
+void processReturn() {}
+
+void processInventory() {}
